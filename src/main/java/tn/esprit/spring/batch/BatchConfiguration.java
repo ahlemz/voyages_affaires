1270ba5f16fb8a20aaf9641fb96bd7f808b6e401
Aybenaziza <ayoub.benaziza@esprit.tn>
2022-07-17 16:54:06 +0200

    spring Batch matching

---
+++ src/main/java/tn/esprit/spring/batch/BatchConfiguration.java
@@ -0,0 +1,52 @@
+package tn.esprit.spring.batch;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import net.bytebuddy.asm.MemberSubstitution.Substitution.Chain.Step;
+import tn.esprit.spring.entities.Invitation;
+
+// tag::setup[]
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	// end::setup[]
+
+	
+
+	@Bean
+	public InvitationItemProcessor processor() {
+		return new InvitationItemProcessor();
+	}
+
+	@Bean
+	public JdbcBatchItemWriter<Invitation> writer(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Invitation>()
+			.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+			.sql("INSERT INTO invitation (id,dateInvitation, confirmationInvitation,answer,description) VALUES (:id,:dateInvitation, :confirmationInvitation,:answer,:description)")
+			.dataSource(dataSource)
+			.build();
+	}
+	
+}
